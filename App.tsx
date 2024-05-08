357f7859efab55570b0d1898b4e177aad9ab9eae
Wajahat Banday <dev@wajahat.io>
2024-05-09 01:04:07 +0530

    Modal, NavBar, OnPress

--- App.tsx
+++ App.tsx
@@ -1,118 +1,164 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {useState} from 'react';
 import {
+  Button,
+  Image,
+  Modal,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import {NavBarData} from './constants';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [navItem, setNavItem] = useState('');
+  const [modal, setModal] = useState(false);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handlePress = () => {
+    setCount(count + 1);
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ff5454',
+      }}>
+      <SafeAreaView style={styles.safeArea}></SafeAreaView>
+      <View style={styles.centerBox}>
+        <Text style={styles.text}>Welcome, SuperUser!</Text>
+      </View>
+      <View style={styles.centerBox}>
+        <Image
+          source={{uri: 'https://picsum.photos/200'}}
+          style={styles.image1}
+        />
+        {/* <Image source={require('./public/home.png')} style={styles.image1} /> */}
+      </View>
+      <View style={styles.centerBox}>
+        <Text style={styles.text}>{`Count is ${count}`}</Text>
+        <View style={styles.button}>
+          <Button onPress={handlePress} title="Count++" />
+        </View>
+      </View>
+      <View style={styles.centerBox}>
+        <Text>{`You clicked on ${navItem}`}</Text>
+      </View>
+      <View style={styles.fixedView}>
+        <View style={styles.row}>
+          {NavBarData.map((icon, index) => (
+            <TouchableOpacity
+              onPress={() => setNavItem(icon.label)}
+              key={index}>
+              <View style={styles.navItem}>
+                <Image
+                  source={navItem === icon.label ? icon.activeIcon : icon.icon}
+                  style={{
+                    width: 32,
+                    height: 32,
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+      <View style={styles.button}>
+        <Button onPress={() => setModal(prev => !prev)} title="Modal" />
+      </View>
+      {modal ? (
+        <View style={styles.fullScreen}>
+          <View style={styles.modal}>
+            <View style={styles.close}>
+              <Button
+                title="X"
+                onPress={() => setModal(false)}
+                color={'#ff5454'}
+              />
+            </View>
+          </View>
+        </View>
+      ) : null}
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  close: {position: 'absolute', top: 5, right: 5},
+  fullScreen: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  modal: {
+    width: '80%',
+    height: '70%',
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {width: 5, height: 5},
+    borderRadius: 15,
+    borderWidth: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  navItem: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  row: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 15,
+  },
+  fixedView: {
+    display: 'flex',
+    width: '100%',
+    height: 80,
+    backgroundColor: '#f9a602',
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 0,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    backgroundColor: '#ff5454',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+  },
+  safeArea: {
+    marginBottom: 30,
+    backgroundColor: '#f9a602',
+  },
+  centerBox: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    gap: 10,
+  },
+  text: {
+    color: '#000',
+  },
+  image1: {
+    width: 60,
+    height: 60,
+    borderRadius: 999,
   },
 });
-
-export default App;
---
+++ constants.ts
@@ -0,0 +1,22 @@
+export const NavBarData = [
+  {
+    label: 'Home',
+    icon: require('./images/home.png'),
+    activeIcon: require('./images/search.png'),
+  },
+  {
+    label: 'Search',
+    icon: require('./images/search.png'),
+    activeIcon: require('./images/home.png'),
+  },
+  {
+    label: 'Discover',
+    icon: require('./images/discover.png'),
+    activeIcon: require('./images/profile.png'),
+  },
+  {
+    label: 'Profile',
+    icon: require('./images/profile.png'),
+    activeIcon: require('./images/discover.png'),
+  },
+];
